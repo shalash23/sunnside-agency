6af9936d0fc73455f1e95ac0f3a6e7d22e68a774
shalash23 <shalashmohamed232@outlook.com>
2022-12-13 10:59:56 +0200

    adjusting everything

--- script.js
+++ script.js
@@ -4,47 +4,60 @@ const navigationMenu = document.getElementById("navigation-menu");
 const hamburgerIcon = document.querySelector(".hamburger-menu");
 gsap.registerPlugin(ScrollTrigger);
 
-sections.forEach((section) => {
-  gsap.from(section, {
-    scrollTrigger: section,
-    x: "random(-100, 100, 5)",
-    y: "random(-100, 100, 5)",
-    duration: 1,
-    stagger: 0.5,
-    ease: "power2.out",
-  });
-});
+// sections.forEach((section) => {
+//   gsap.from(section, {
+//     scrollTrigger: section,
+//     x: "random(-100, 100, 5)",
+//     y: "random(-100, 100, 5)",
+//     duration: 1,
+//     stagger: 0.5,
+//     ease: "power2.out",
+//   });
+// });
 
-footer.forEach((foot) => {
-  gsap.from(foot, {
-    scrollTrigger: foot,
+// footer.forEach((foot) => {
+//   gsap.from(foot, {
+//     scrollTrigger: foot,
 
-    opacity: 0.3,
-    skewY: "30deg",
-    rotation: 80,
-    ease: Power0.easeIn,
-    duration: 0.5,
-    stagger: 0.5,
-  });
-});
+//     opacity: 0.3,
+//     skewY: "30deg",
+//     rotation: 80,
+//     ease: Power0.easeIn,
+//     duration: 0.5,
+//     stagger: 0.5,
+//   });
+// });
 
-hamburgerIcon.addEventListener("click", function () {
-  if (navigationMenu.style.opacity === "1") {
-    navigationMenu.style.opacity === "0";
-    gsap.to(navigationMenu, {
-      opacity: 0,
-      duration: 0.5,
-    });
+// hamburgerIcon.addEventListener("click", function () {
+//   if (navigationMenu.style.opacity === "1") {
+//     navigationMenu.style.opacity === "0";
+//     gsap.to(navigationMenu, {
+//       opacity: 0,
+//       duration: 0.5,
+//     });
+//     return;
+//   }
+//   navigationMenu.style.opacity === "1";
+//   gsap.to(navigationMenu, {
+//     opacity: 1,
+//     y: 50,
+//     duration: 0.5,
+//   });
+//   return;
+
+//   // navigationMenu.classList.remove("is-hidden");
+//   // navigationMenu.classList.add("is-active");
+// });
+
+hamburgerIcon.addEventListener("click", () => {
+  if (navigationMenu.classList.contains("is-hidden")) {
+    navigationMenu.classList.add("is-active");
+    navigationMenu.classList.remove("is-hidden");
     return;
   }
-  navigationMenu.style.opacity === "1";
-  gsap.to(navigationMenu, {
-    opacity: 1,
-    y: 50,
-    duration: 0.5,
-  });
-  return;
-
-  // navigationMenu.classList.remove("is-hidden");
-  // navigationMenu.classList.add("is-active");
+  if (navigationMenu.classList.contains("is-active")) {
+    navigationMenu.classList.remove("is-active");
+    navigationMenu.classList.add("is-hidden");
+    return;
+  }
 });
